20860630548e94064144467baa55085691f3db1f
David Bakshi <davidbakshi1@gmail.com>
2025-08-08 15:03:36 +0300

    remove unnecessary code

--- audio_model_training/yamnet_audio_finetune.py
+++ audio_model_training/yamnet_audio_finetune.py
@@ -188,45 +188,4 @@ serving_model.save(saved_model_path, include_optimizer=False)
 
 
 
-
-
-
-
-
 tf.keras.utils.plot_model(serving_model)
-
-reloaded_model = tf.saved_model.load(saved_model_path)
-
-# reloaded_results = reloaded_model(testing_wav_data)
-# cat_or_dog = my_classes[tf.math.argmax(reloaded_results)]
-# print(f'The main sound is: {cat_or_dog}')
-
-serving_results = reloaded_model.signatures['serving_default'](testing_wav_data)
-cat_or_dog = my_classes[tf.math.argmax(serving_results['classifier'])]
-print(f'The main sound is: {cat_or_dog}')
-
-
-test_pd = filtered_pd.loc[filtered_pd['fold'] == 5]
-row = test_pd.sample(1)
-filename = row['filename'].item()
-print(filename)
-waveform = load_wav_16k_mono(filename)
-print(f'Waveform values: {waveform}')
-_ = plt.plot(waveform)
-
-display.Audio(waveform, rate=16000)
-
-# Run the model, check the output.
-scores, embeddings, spectrogram = yamnet_model(waveform)
-class_scores = tf.reduce_mean(scores, axis=0)
-top_class = tf.math.argmax(class_scores)
-inferred_class = class_names[top_class]
-top_score = class_scores[top_class]
-print(f'[YAMNet] The main sound is: {inferred_class} ({top_score})')
-
-reloaded_results = reloaded_model(waveform)
-your_top_class = tf.math.argmax(reloaded_results)
-your_inferred_class = my_classes[your_top_class]
-class_probabilities = tf.nn.softmax(reloaded_results, axis=-1)
-your_top_score = class_probabilities[your_top_class]
-print(f'[Your model] The main sound is: {your_inferred_class} ({your_top_score})')
